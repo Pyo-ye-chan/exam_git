7b823ceec24967e41d3d1e9a3fe3ebdf73a94003
Seol <chlo01233@gmail.com>
2026-05-08 16:45:00 +0900

    board list updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import List from './domains/board/List';
+import Detail from './domains/board/Detail'; // Import the Detail component
+import Login from './domains/members/Login'; // Keep Login if it's intended to be there
 import './App.css'; // Keep the App.css import if there are global styles
-
-import Login from './domains/members/Login';
-import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-
-      <Routes>
-        <Route index element={<Login />}/>
-        <Route path="/board">
-          <Route path="list" element={<List />} />
-        
-        </Route>
-
-      </Routes>
-
-    </div>
+   
+      <div className="App">
+        <Routes>
+          <Route index element={<Login />}/>
+          <Route path="/board">
+            <Route path="list" element={<List />} />
+            <Route path=":seq" element={<Detail />} /> {/* New route for board detail */}
+          </Route>
+        </Routes>
+      </div>
+  
   );
 }
 
--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -3,3 +3,13 @@ import axios from 'axios';
 export const maxios = axios.create({
     baseURL : `http://localhost`
 });
+
+// maxios.interceptors.request.use((config) => {
+//     const token = sessionStorage.getItem("token");
+//     console.log("인터셉터:"+token)
+//     if(token){
+//         config.headers["Authorization"] = `Bearer ${token}`
+//     }
+//     console.log(token);
+//     return config;
+// });
